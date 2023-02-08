775b4b542689a956fad07229a20881d959daa337
Mike Ward <michaelward@google.com>
2023-02-08 04:07:18 -0800

    Create empty row for post processing metrics to prevent failure if no new metrics have run.
    PiperOrigin-RevId: 508039597
    Change-Id: I06506e181df55e87c0c7e769db00c9cca391a791

--- macros/metric_processing_macros/union_metric_tables.sql
+++ macros/metric_processing_macros/union_metric_tables.sql
@@ -4,14 +4,16 @@
     {%- endset -%}
     {%- if execute -%}
         {%- set metrics = run_query(metric_tables).columns[0].values() -%}
+        SELECT * FROM ({{ empty_metric_output() }})
+        WHERE 1=0
         {%- for metric in metrics -%}
             {%- set relation = adapter.get_relation(
                   database = target.project,
                   schema = target.dataset,
                   identifier = metric
             ) %}
+            UNION ALL
             SELECT * FROM {{ relation.database }}.{{ relation.schema }}.{{ relation.name }}
-            {% if not loop.last -%}  UNION ALL {%- endif -%}
         {%- endfor -%}
     {%- endif -%}
 {%- endmacro -%}
