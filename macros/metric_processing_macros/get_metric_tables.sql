53f2036fb03106d40e2c93b8ad178a43873c9f88
Jakub Adamek <jakuba@google.com>
2023-03-03 03:19:22 -0800

    Modify metric_definition to only include metrics which were executed
    Create metric_all_definitions to cover the full list.
    
    PiperOrigin-RevId: 513781924
    Change-Id: Ifabe66aa260708ebdf5dcd41b11296e2ece9a56b

--- macros/metric_processing_macros/get_metric_tables.sql
+++ macros/metric_processing_macros/get_metric_tables.sql
@@ -3,5 +3,5 @@
     T.table_name,
     CONCAT('`', T.table_catalog, '`.`', T.table_schema, '`.`', T.table_name, '`') AS fully_qualified_bq_table
   FROM {{target.project}}.{{target.dataset}}.INFORMATION_SCHEMA.TABLES AS T
-  JOIN {{ ref('metric_definition') }} AS D ON T.table_name = D.metric_name
+  JOIN {{ ref('metric_all_definitions') }} AS D ON T.table_name = D.metric_name
 {%- endmacro %}
