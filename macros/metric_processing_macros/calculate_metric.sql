07a3106d44c81faa1198063fd9c3ea22bb9aa8fb
Jakub Adamek <jakuba@google.com>
2023-03-16 09:45:58 -0700

    Print why metric empty to help debugging
    It can be useful to see which piece of data was missing or if the metric configuration was wrong.
    
    PiperOrigin-RevId: 517147955
    Change-Id: I37653563487a02b46e36a8cef6abf530aeb84243

--- macros/metric_processing_macros/calculate_metric.sql
+++ macros/metric_processing_macros/calculate_metric.sql
@@ -13,19 +13,30 @@ WITH
 
 
 {# Check that all inputs are available. #}
-{%- macro _are_inputs_available() %}
-    {% if not fhir_resource_exists(model_metadata('primary_resource')) %}
-        {% do return(False) %}
-    {% endif %}
-    {% for secondary_resource in model_metadata('secondary_resources', value_if_missing=[])
-        if not fhir_resource_exists(secondary_resource) %}
-        {% do return(False) %}
-    {% endfor %}
-    {% for primary_field in model_metadata('primary_fields', value_if_missing=[])
-        if not column_exists(primary_field) %}
-        {% do return(False) %}
-    {% endfor %}
+{% macro _are_inputs_available() %}
+  {% if not fhir_resource_exists(model_metadata('primary_resource')) %}
+    {{ _print_why("primary resource " ~ model_metadata('primary_resource')) }}
+    {% do return(False) %}
+  {% endif %}
+  {% for secondary_resource in model_metadata('secondary_resources', value_if_missing=[])
+      if not fhir_resource_exists(secondary_resource) %}
+    {{ _print_why("secondary resource " ~ secondary_resource) }}
+    {% do return(False) %}
+  {% endfor %}
+  {% for primary_field in model_metadata('primary_fields', value_if_missing=[])
+      if not column_exists(primary_field) %}
+    {{ _print_why("primary field " ~ primary_field) }}
+    {% do return(False) %}
+  {% endfor %}
 
-    {% do return(True) %}
-{% endmacro -%}
+  {% do return(True) %}
+{% endmacro %}
+
+
+{# Print why is the metric empty, which input was not available. #}
+{% macro _print_why(what_is_missing) %}
+  {% if var('print_why_metric_empty') and execute %}
+    {{ print(this.name ~ " empty because " ~ what_is_missing ~ " is not available") }}
+  {% endif %}
+{% endmacro %}
 
