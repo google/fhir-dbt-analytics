e0d3c5a7e9f0fd9a87ab082a87b9e21cd01e9650
Tara Ojo <taraojo@google.com>
2023-08-04 08:59:07 -0700

    Add Apache License to all files
    PiperOrigin-RevId: 553821931
    Change-Id: I3770f5c0075084fbf9cd1eb21b09b6b964665fcb

--- macros/cohort_macros/has_observation.sql
+++ macros/cohort_macros/has_observation.sql
@@ -1,3 +1,17 @@
+-- Copyright 2023 Google LLC
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
 {% macro has_observation(observation, code_system=None, value_greater_than=None, value_less_than=None, lookback=None, patient_join_key= None) -%}
 EXISTS (
   SELECT cc.code
--- macros/fhir_view_macros/query_fhir_table.sql
+++ macros/fhir_view_macros/query_fhir_table.sql
@@ -1,3 +1,17 @@
+-- Copyright 2023 Google LLC
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
 {%- macro query_fhir_table(fhir_resource) -%}
     {%- set metric_date_columns = get_metric_date_columns() -%}
     {%- set date_column_data_type = column_data_type(metric_date_columns[0], get_source_table_name(fhir_resource)) %}
