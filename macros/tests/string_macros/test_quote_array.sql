4e31f211d0beb0a799776bbcff894333325b800b
Sophia Botz <sophiabotz@google.com>
2023-10-11 09:09:05 -0700

    Simplify quote_array macro test. The bigquery and spark behaviour is now the same since updates in dbt-spark v1.4.0
    PiperOrigin-RevId: 572593143
    Change-Id: I42f9029ef39e77eb44dedda32855f3522fb2d953

--- macros/tests/string_macros/test_quote_array.sql
+++ macros/tests/string_macros/test_quote_array.sql
@@ -16,16 +16,4 @@
   {{ dbt_unittest.assert_equals(quote_array([]), []) }}
   {{ dbt_unittest.assert_equals(quote_array(["foo"]), ["'foo'"]) }}
   {{ dbt_unittest.assert_equals(quote_array(["foo", "bar"]), ["'foo'", "'bar'"]) }}
-
-  {{ return(adapter.dispatch('test_quote_array', 'fhir_dbt_analytics') ()) }}
-{%- endmacro %}
-
-
-{% macro default__test_quote_array() -%}
-  {{ dbt_unittest.assert_equals(quote_array(["f'o'o"]), ["'f\\'o\\'o'"]) }}
-{%- endmacro %}
-
-
-{% macro spark__test_quote_array() -%}
-  {{ dbt_unittest.assert_equals(quote_array(["f'o'o"]), ["'f''o''o'"]) }}
 {%- endmacro %}
